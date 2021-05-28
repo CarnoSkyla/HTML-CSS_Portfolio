1163619d1842526d5732cbc44ba5196dab34b0d6
Sibusiso Labi <sibusiso@quirky30.co.za>
2021-05-28 22:20:51 +0200

    Restructed and updated

--- practice.js
+++ practice.js
@@ -1,3 +1,39 @@
-var name = "Sbucarno";
-var surname = "Labi";
-document.getElementById('prac').innerHTML = name + surname
+var username = "Sbucarno";
+var usersurname = "Labi";
+
+const isUserNameCorrect = false;
+
+ checkName = (name, surname) => {
+    var users = [];
+    var errors = [];
+    
+    if ((typeof name == 'string' && typeof surname == 'string')) {
+        const userFullName = name.concat(' ' + surname);
+        const name = userFullName.split('');
+        if (name.contains(Number)) {
+            errors.push({
+                error: `${name} ${surname} contains a number`
+            })
+            isUserNameCorrect = false;
+            errors.forEach(error => {
+                document.getElementById('prac').innerHTML = `${error}, try again`
+            });
+        } else {
+            users.push({
+                user: userFullName
+            })
+            errors = []
+            isUserNameCorrect = true;
+        } 
+    } else {
+        isUserNameCorrect = false
+        errors.push({
+            error: `${name} ${surname} is not a valid name`
+        })
+        errors.forEach(error => {
+            document.getElementById('prac').innerHTML = `${error}, try again`
+        });
+    }
+    return users;
+}
+//document.getElementById('prac').innerHTML = name + surname
